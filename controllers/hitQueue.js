43deb1018f0c79adfedc9c6823cb4bb3ed2c6de9
Sebastian Wegmann <WEGMASE@de.ibm.com>
2016-03-11 10:00:46 +0100

    Use correct method to display the elements in the array.

--- controllers/hitQueue.js
+++ controllers/hitQueue.js
@@ -7,7 +7,7 @@
  */
 
 var queue = [];
-
+var wellOrdered;
 
 
 
@@ -21,33 +21,42 @@ module.exports = {
 };
 
 
+// *****
+// Compare Function for hits (should allow sort an array of hits by sequence Count)
+// *****
+function hitComparator(hitA, hitB) {
+    // console.log("DEBUG: hitComparator called with " +  JSON.stringify(hitA) + " and " + JSON.stringify(hitB));
+    return parseInt(hitA.seqCount) - parseInt(hitB.seqCount);
+}
 
 /*==============================================================================
  * Functions
  *==============================================================================
  */
+
+// *****
+// Include a new hit in the queue and trigger follow-up events 
+// *****
 function newHit(ttHit) {
 
     console.log("DEBUG: Entering newHit\n");
 
-    console.log("DEBUG: Pushing into queue. queue has " + queue.lastIndexOf + " elements.");
+    console.log("DEBUG: Pushing into queue. queue has " + queue.length + " elements.");
     queue.push(ttHit);
 
-    console.log("DEBUG: Sorting the queue. The queue has " + queue.lastIndexOf + " elements.");
+    console.log("DEBUG: Sorting the queue. The queue has " + queue.length + " elements.");
     queue.sort(hitComparator);
 
-    console.log("DEBUG: Printing the queue: " + queue);
+    //console.log("DEBUG: Printing the queue: " + JSON.stringify(queue, null, 4));
+    //console.log("DEBUG: Leaving newHit\n");
+}
 
 
-    console.log("DEBUG: Leaving newHit\n");
+function evaluateQueueStability() {
+    // auswerten ob die queue gerade stabil ist
+
+    // spielzug auswertung
 }
 
 
 
-
-
-
-// Compare Function for hits (should allow sort an array of hits by sequence Count)
-function hitComparator(hitA, hitB) {
-    return parseInt(hitA.secCount) - parseInt(hitB.secCount);
-}
